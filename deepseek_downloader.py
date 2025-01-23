92c423ce6cc5a8419f0f54e846f3cbf63bcd284e
Michael Böhme <boehme.mic@gmail.com>
2025-01-23 21:08:02 +0100

    reduced RAM usage

--- deepseek_downloader.py
+++ deepseek_downloader.py
@@ -19,5 +19,8 @@ for repo in REPOS:
     if os.path.exists(repo):
         print(f"Skipping {repo} as it already exists")
         continue
-    print(f"Cloning {repo}...")
-    os.system(f"git clone {URL}")
+    print(f"Cloning {repo}...")    
+    # checkout the repo w/o LFS files first
+    # then fetch and checkout LFS files
+    os.system(f"GIT_LFS_SKIP_SMUDGE=1 git clone {URL} && cd {repo} && git lfs fetch && git lfs checkout")
+
